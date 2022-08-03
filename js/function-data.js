10c60ac3d9d2e6aca759d044c36a3d9c2bb30f22
widiopranoto <widiopranoto@gmail.com>
2022-08-03 11:06:07 +0700

    second commit

--- js/function-data.js
+++ js/function-data.js
@@ -1,14 +1,15 @@
 var inputUrlSpreadSheet="https://docs.google.com/spreadsheets/d/1S1kEhlCAi1N48CR98eW-OLzgmGgjRYjIYxWPLJ9cCt8/edit?usp=sharing";
 var inputIdGScript='AKfycbwwQpjerLK3moZDdr8W_ksxt8el6Ay77I7cjwIoVqpWXJSq0q0';
 let username,password,usernameOnline,passwordOnline,nama,status,tingkat,inputNamaSheet;
+let usernameOffline,passwordOffline,statusOnline;
+usernameOffline=localStorage.getItem('usernameOffline');
 tingkat=localStorage.getItem('tingkat');
 if(tingkat=='X'){inputNamaSheet='X';}
 if(tingkat=='XI'){inputNamaSheet='XI';}
 if(tingkat=='XII'){inputNamaSheet='XII';}
 if(tingkat=='XIII'){inputNamaSheet='XIII';}
 
-
-function ambilItem(inputNamaSheet,username,noKolom,idLokal,idTampil){
+function ambilItem(inputNamaSheet,usernameOffline,noKolom,idLokal,idTampil){
   var action="getItem";
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
@@ -28,7 +29,7 @@ function ambilItem(inputNamaSheet,username,noKolom,idLokal,idTampil){
   var teks8="&action=";
   var teks9="getItem";
   var teks10="&username=";
-  var teks11=username;
+  var teks11=usernameOffline;
   var teks12="&noKolom=";
   var teks13=noKolom;
   var teksGabung=teks1+teks2+teks3+teks4+teks5+teks6+teks7+teks8+teks9+teks10+teks11+teks12+teks13;
@@ -41,13 +42,12 @@ function showItem(simpanLokal,idTampil){
   document.getElementById(idTampil).innerHTML=simpanLokal;
 }
 function cekStatus(){
-  usernameOnline=localStorage.getItem('usernameOnline');
+  usernameOffline=localStorage.getItem('usernameOffline');
   nama=localStorage.getItem('nama');
   status=localStorage.getItem('status');
   if(status!='logged-in'){
-	localStorage.setItem('username',null);
-	localStorage.setItem('password',null);
-    localStorage.setItem('usernameOnline',null);
+	localStorage.setItem('usernameOffline',null);
+	localStorage.setItem('passwordOffline',null);
 	localStorage.setItem('nama',null);
 	//localStorage.setItem('status','Anda belum login');
   }
@@ -59,7 +59,7 @@ function cekStatus(){
   }
 }
 function tampilkanStatus(idTampil1,idTampil2,idTampil3){
-	usernameOnline=localStorage.getItem('usernameOnline');
+	usernameOffline=localStorage.getItem('usernameOffline');
     nama=localStorage.getItem('nama');
     status=localStorage.getItem('status');
 	showItem(usernameOnline,idTampil1);
@@ -71,8 +71,15 @@ function isikanLogin(){
   usernameIsian = document.getElementById('username').value;
   passwordIsian = document.getElementById('password').value; 
   tingkatIsian = document.getElementById('tingkat').value; 
+  //isikan username, password, dan tingkat ke local storage
+  localStorage.setItem('usernameOffline',usernameIsian);
+  localStorage.setItem('passwordOffline',passwordIsian);	  
   localStorage.setItem('tingkat',tingkatIsian);
-  tingkat=localStorage.getItem('tingkat');
+  //ambil username, password, dan tingkat dari local storage
+  //usernameOffline=localStorage.getItem('usernameOffline');
+  //passwordOffline=localStorage.getItem('passwordOffline');
+  //tingkat=localStorage.getItem('tingkat');
+  /*
   if(status!='logged-in' && usernameIsian!=null){
     localStorage.setItem('username',usernameIsian);
     localStorage.setItem('password',passwordIsian);	
@@ -81,36 +88,53 @@ function isikanLogin(){
     username=localStorage.getItem('username');
     password=localStorage.getItem('password');	
   }  
+  */
 }  
 function cekLoginOnline(){
-  username = localStorage.getItem('username');
-  password = localStorage.getItem('password');
+  //ambil usernameOffline dan passwordOffline
+  usernameOffline = localStorage.getItem('usernameOffline');
+  passwordOffline = localStorage.getItem('passwordOffline');
+  tingkat=localStorage.getItem('tingkat');
   //ambil username dan password di cloud
-  ambilItem(inputNamaSheet,username,1,'usernameOnline');
-  ambilItem(inputNamaSheet,username,2,'nama');
-  ambilItem(inputNamaSheet,username,3,'passwordOnline');
+  ambilItem(inputNamaSheet,usernameOffline,1,'usernameOnline');
+  ambilItem(inputNamaSheet,usernameOffline,2,'nama');
+  ambilItem(inputNamaSheet,usernameOffline,3,'passwordOnline');
 }
 function validasiLogin(){
 //cek jika usernameOnline dan passwordOnline sudah tersedia
 //if(usernameOnline!=null && passwordOnline!=null){
-    username=localStorage.getItem('username');
-    password=localStorage.getItem('password');
+    usernameOffline=localStorage.getItem('usernameOffline');
+    passwordOffline=localStorage.getItem('passwordOffline');
     usernameOnline=localStorage.getItem('usernameOnline');
     passwordOnline=localStorage.getItem('passwordOnline');  
-	if(username!=usernameOnline || password!=passwordOnline){localStorage.setItem('status','Anda belum login (validasi)');}
-	if(username==usernameOnline && password==passwordOnline){localStorage.setItem('status','logged-in');}
+	//if(usernameOffline!=usernameOnline || passwordOffline!=passwordOnline){localStorage.setItem('status','Anda belum login (validasi)');}
+	//if(usernameOffline==usernameOnline && passwordOffline==passwordOnline){localStorage.setItem('status','logged-in');}
+	var lengthPassword=localStorage.getItem('passwordOffline').length;
+	if(usernameOffline!=null && passwordOffline!=null && lengthPassword==8){localStorage.setItem('status','logged-in');}
+// }
+}
+function validasiLoginLevel2(){
+//cek jika usernameOnline dan passwordOnline sudah tersedia
+//if(usernameOnline!=null && passwordOnline!=null){
+    usernameOffline=localStorage.getItem('usernameOffline');
+    passwordOffline=localStorage.getItem('passwordOffline');
+    usernameOnline=localStorage.getItem('usernameOnline');
+    passwordOnline=localStorage.getItem('passwordOnline');  
+	if(usernameOffline!=usernameOnline || passwordOffline!=passwordOnline){localStorage.setItem('statusOnline',1);}
+	if(usernameOffline==usernameOnline && passwordOffline==passwordOnline){localStorage.setItem('statusOnline',0);}
 // }
 }
 function logOut(){
   localStorage.clear();
-  localStorage.setItem('status','Anda belum login');
+  localStorage.setItem('status','Anda belum login (sudah logout)');
+  showItem(status,'status_hal_beranda');
 }
 function cekLogoutHalLogin(){
 	status=localStorage.getItem('status');
     if(status!='logged-in'){
 	document.getElementById('username_hal_login').innerHTML=null; 	  
 	document.getElementById('nama_hal_login').innerHTML=null; 	  
-	document.getElementById('status_hal_login').innerHTML='Anda belum login'; 	  
+	document.getElementById('status_hal_login').innerHTML='Anda belum login (cek logout)'; 	  
 	}
   if(localStorage.getItem('status')=='logged-in'){
     usernameOnline=localStorage.getItem('usernameOnline');
@@ -130,6 +154,33 @@ document.write(
 "<button type='submit' onclick="+"simpanItem("+"'"+idData+"'"+")"+">Simpan</button>"+
 "</div>"+
 "</form>"+
+"</div>");
+}
+function tulisModalStatus(idModal){
+document.write(
+"<div id="+"'"+idModal+"'"+" class='modal'>"+
+"<div class='modal-content animate'>"+
+"<div class='container'>"+
+"<span onclick="+"document.getElementById("+"'"+idModal+"'"+").style.display='none'"+" class="+"close"+">&times;</span>"+
+"<label><b>Status Login</b></label>"+
+"<div class='col-sm-12 text-left'>"+
+"<table class='w3-table-all'>"+
+"<thead>"+
+"<tr class='w3-blue'>"+
+"<th>Username/NIS</th>"+
+"<th>Nama</th>"+
+"<th>Status Login</th>"+
+"</tr>"+
+"</thead>"+
+"<tr>"+
+"<td id='username_modal_login'></td>"+
+"<td id='nama_modal_login'></td>"+
+"<td id='status_modal_login'></td>"+
+"</tr>"+
+"</table>"+
+"</div>"+
+"</div>"+
+"</div>"+
 "</div>");
 }
 function simpanItem(idData){
@@ -178,7 +229,7 @@ function simpanItem(idData){
   if(idData=='editJmlSaudaraKandung')noKolom=44;
  
   var action="postItem";
-  username=localStorage.getItem('usernameOnline');
+  usernameOffline=localStorage.getItem('usernameOffline');
   var item = document.getElementById(idData).value;
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
@@ -198,7 +249,7 @@ function simpanItem(idData){
   var teks8="&action=";
   var teks9="postItem";
   var teks10="&username=";
-  var teks11=username;
+  var teks11=usernameOffline;
   var teks12="&noKolom=";
   var teks13=noKolom;
   var teks14="&item=";
